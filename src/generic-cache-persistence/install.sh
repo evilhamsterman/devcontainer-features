6f52771ac1e28e09c518b5cdb4f7311c945ff725
Dan Mills <dmills@qumulo.com>
2023-09-08 23:46:15 +0000

    Successfully works on deb based containers

--- src/generic-cache-persistence/install.sh
+++ src/generic-cache-persistence/install.sh
@@ -1,40 +1,49 @@
 #!/bin/sh
 set -e
 
-HOME=$_REMOTE_USER_HOME
+echo "Remote User ${_REMOTE_USER}"
+echo "Remote Home ${_REMOTE_USER_HOME}"
+
+cache_maps=$_REMOTE_USER_HOME/.cache_maps
 
 cache_dir () {
-  echo "Creating cache folder for $1"
-  dir_hash=$(echo $1 | sha1sum --zero | cut -f1 -d" ")
-  hashed_path=$CACHE_HOME/$dir_hash
-  if [ -e $1 ]; then
-    echo "Moving existing folder $1 to $1-old"
-    mv $1 $1-old
+  home_path=$_REMOTE_USER_HOME/$1
+  echo "Creating cache folder for $home_path"
+  if [ -d $home_path ]; then
+    mv $home_path $home_path-old
   fi
-  mkdir $hashed_path
-  ln -s $hashed_path $1
-  chown -R $_REMOTE_USER:$_REMOTE_USER $1
-  echo "$1 $hashed_path" >> $_REMOTE_USER_HOME/.cache_maps
+  dir_hash=$(echo $home_path | sha1sum --zero | cut -f1 -d" ")
+  hashed_path=$CACHE_HOME/$dir_hash
+  ln -sf $hashed_path $home_path
+  chown -R $_REMOTE_USER:$_REMOTE_USER $home_path
+  echo "$home_path:$hashed_path" >> $cache_maps
 }
 
 for path in $CACHE_VOLUMES; do
   cache_dir $path
 done
 
-cat << EOF >> $_REMOTE_USER_HOME/.bashrc
-sudo chown -R $_REMOTE_USER:$_REMOTE_USER $CACHE_HOME
+cat << EOF >> $_REMOTE_USER_HOME/.persistence
+#!/bin/sh
+set -e
+echo "Finalizing persistence"
+echo "Create persistence folders"
+
+while IFS=: read -r cached_path hashed_path; do
+  if [ \$(id -u) != 0 ]; then
+    if ! test -d \$hashed_path; then
+      echo "Creating \$hashed_path"
+      sudo mkdir \$hashed_path
+    fi
+    echo "chowning \$hashed_path"
+    sudo chown -R "$_REMOTE_USER:$_REMOTE_USER" \$hashed_path
+  else
+    if ! test -d \$hashed_path; then
+      mkdir \$hashed_path
+    fi
+    chown -R "$_REMOTE_USER:$_REMOTE_USER" \$hashed_path
+  fi
+done < $cache_maps
+
 EOF
-
-# if [ -e "$_REMOTE_USER_HOME/.azure" ]; then
-#   echo "Moving existing .azure folder to .azure-old"
-#   mv "$_REMOTE_USER_HOME/.azure" "$_REMOTE_USER_HOME/.azure-old"
-# fi
-
-# ln -s /dc/azure/ "$_REMOTE_USER_HOME/.azure"
-# chown -R "${_REMOTE_USER}:${_REMOTE_USER}" "$_REMOTE_USER_HOME/.azure"
-
-# # chown mount (only attached on startup)
-# cat << EOF >> "$_REMOTE_USER_HOME/.bashrc"
-# sudo chown -R "${_REMOTE_USER}:${_REMOTE_USER}" /dc/azure
-# EOF
-# chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.bashrc
+chmod +x $_REMOTE_USER_HOME/.persistence
--- test/generic-cache-persistence/test.sh
+++ test/generic-cache-persistence/test.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 source dev-container-features-test-lib
+
+check "Check $HOME/.config is a link" test -L $HOME/.config
+check "Check $HOME/.config links to $CACHE_HOME" bash -c "readlink $HOME/.config | grep -q $CACHE_HOME"
+
+reportResults
